37f4791a8fedbdabc0c47c9b9202b248822782dd
Arbaz Shaikh <shaikharbaz077@gmail.com>
2023-02-05 19:18:57 +0530

    Fix design of search screen

--- lib/feature/presentation/pages/search/news_search.dart
+++ lib/feature/presentation/pages/search/news_search.dart
@@ -6,7 +6,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_news_app/dependency_injection.dart';
 import 'package:flutter_news_app/feature/presentation/bloc/bloc.dart';
-import 'package:flutter_news_app/feature/presentation/bloc/top_headline_bloc.dart';
 import 'package:flutter_news_app/feature/presentation/pages/widget/widget_failure_message.dart';
 import 'package:flutter_news_app/feature/presentation/pages/widget/widget_item_news.dart';
 
@@ -46,134 +45,121 @@ class _SearchPageState extends State<SearchPage> {
 
   @override
   Widget build(BuildContext context) {
-    var theme = Theme.of(context);
-    var isDarkTheme = theme.brightness == Brightness.dark;
     return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        // leadingWidth: ,
+        automaticallyImplyLeading: false,
+
+        title: Row(
+          children: [
+            InkWell(
+                onTap: () {
+                  Navigator.of(context).pop();
+                },
+                child: Icon(
+                    Platform.isIOS ? Icons.arrow_back_ios : Icons.arrow_back)),
+            Expanded(
+              child: TextField(
+                showCursor: true,
+                focusNode: focusNodeIconSearch,
+                cursorColor: Colors.white,
+                controller: controllerKeyword,
+                decoration: InputDecoration(
+                  isDense: true,
+                  hintText: 'Search for topics, locations & sources',
+                  hintStyle: TextStyle(
+                    fontSize: 16.sp,
+                    color: Colors.white60,
+                  ),
+                  // border: OutlineInputBorder()
+                  enabledBorder: InputBorder.none,
+                  focusedBorder: InputBorder.none,
+                ),
+                style: TextStyle(fontSize: 16.sp, color: Colors.white),
+              ),
+            ),
+            IconButton(
+              icon: const Icon(Icons.search),
+              onPressed: () {
+                focusNodeIconSearch.requestFocus();
+              },
+            ),
+          ],
+        ),
+      ),
       body: BlocProvider<TopHeadlinesNewsBloc>(
         create: (context) => topHeadlinesNewsBloc,
-        child: Stack(
-          children: [
-            Container(
-              width: double.infinity,
-              height: double.infinity,
-              color: isDarkTheme ? null : Color(0xFFEFF5F5),
-            ),
-            Container(
-              color: isDarkTheme ? null : Color(0xFFEFF5F5),
-              width: double.infinity,
-              padding: EdgeInsets.symmetric(
-                vertical: 24.h,
-                horizontal: 10.w,
-              ),
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.pop(context);
-                        },
-                        child: Icon(
-                          Platform.isIOS
-                              ? Icons.arrow_back_ios
-                              : Icons.arrow_back,
-                        ),
-                      ),
-                      SizedBox(width: 10.w),
-                      Expanded(
-                        child: Container(
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Colors.grey),
-                            borderRadius: BorderRadius.circular(20.r),
-                          ),
-                          padding: EdgeInsets.symmetric(horizontal: 15.w),
-                          child: Row(
-                            children: [
-                              Expanded(
-                                child: TextField(
-                                  controller: controllerKeyword,
-                                  decoration: InputDecoration(
-                                    isDense: true,
-                                    hintText: 'Searching something?',
-                                    hintStyle: TextStyle(
-                                      fontSize: 16.sp,
-                                      color: Colors.grey,
-                                    ),
-                                    enabledBorder: InputBorder.none,
-                                    focusedBorder: InputBorder.none,
-                                  ),
-                                  style: TextStyle(
-                                    fontSize: 16.sp,
-                                  ),
-                                ),
+        child: Container(
+          width: double.infinity,
+          padding: EdgeInsets.symmetric(
+            vertical: 24.h,
+            horizontal: 10.w,
+          ),
+          child: Column(
+            children: [
+              SizedBox(height: 16.h),
+              Expanded(
+                child: BlocBuilder<TopHeadlinesNewsBloc, TopHeadlinesState>(
+                  builder: (context, state) {
+                    if (state is LoadingHeadlineState) {
+                      return Center(
+                        child: Platform.isIOS
+                            ? CupertinoActivityIndicator(
+                                color: Colors.white,
+                                radius: 20.r,
+                              )
+                            : const CircularProgressIndicator(
+                                color: Colors.white,
                               ),
-                              Hero(
-                                tag: 'iconSearch',
-                                child: Focus(
-                                  focusNode: focusNodeIconSearch,
-                                  child: Icon(
-                                    Icons.search,
-                                    size: 16.h,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 16.h),
-                  Expanded(
-                    child: BlocBuilder<TopHeadlinesNewsBloc, TopHeadlinesState>(
-                      builder: (context, state) {
-                        if (state is LoadingHeadlineState) {
-                          return Center(
-                            child: Platform.isIOS
-                                ? const CupertinoActivityIndicator()
-                                : const CircularProgressIndicator(),
-                          );
-                        } else if (state is FailureHeadlineState) {
-                          return const WidgetFailureMessage();
-                        } else if (state is SearchedHeadlineState) {
-                          var listArticles = state.listArticles;
-                          if (listArticles.isEmpty) {
-                            return const WidgetFailureMessage(
+                      );
+                    } else if (state is FailureHeadlineState) {
+                      return const Center(
+                        child: SingleChildScrollView(
+                            child: WidgetFailureMessage()),
+                      );
+                    } else if (state is SearchedHeadlineState) {
+                      var listArticles = state.listArticles;
+                      if (listArticles.isEmpty) {
+                        focusNodeIconSearch.unfocus();
+                        return const Center(
+                          child: SingleChildScrollView(
+                            child: WidgetFailureMessage(
                               errorTitle: 'Data not found',
                               errorSubtitle:
                                   'Hm, we couldn\'t find what you were looking for.',
+                            ),
+                          ),
+                        );
+                      } else {
+                        return ListView.builder(
+                          padding: EdgeInsets.zero,
+                          itemBuilder: (context, index) {
+                            var itemArticle = listArticles[index];
+                            var dateTimePublishedAt =
+                                DateFormat('yyy-MM-ddTHH:mm:ssZ')
+                                    .parse(itemArticle.publishedAt ?? "", true);
+                            var strPublishedAt =
+                                DateFormat('MMM dd, yyyy HH:mm')
+                                    .format(dateTimePublishedAt);
+                            return Padding(
+                              padding: EdgeInsets.symmetric(vertical: 16.h),
+                              child: WidgetItemNews(
+                                itemArticle: itemArticle,
+                                strPublishedAt: strPublishedAt,
+                              ),
                             );
-                          } else {
-                            return ListView.builder(
-                              padding: EdgeInsets.zero,
-                              itemBuilder: (context, index) {
-                                var itemArticle = listArticles[index];
-                                var dateTimePublishedAt =
-                                    DateFormat('yyy-MM-ddTHH:mm:ssZ').parse(
-                                        itemArticle.publishedAt ?? "", true);
-                                var strPublishedAt =
-                                    DateFormat('MMM dd, yyyy HH:mm')
-                                        .format(dateTimePublishedAt);
-                                return Padding(
-                                  padding: EdgeInsets.symmetric(vertical: 16.h),
-                                  child: WidgetItemNews(
-                                    itemArticle: itemArticle,
-                                    strPublishedAt: strPublishedAt,
-                                  ),
-                                );
-                              },
-                              itemCount: listArticles.length,
-                            );
-                          }
-                        }
-                        return Container();
-                      },
-                    ),
-                  ),
-                ],
+                          },
+                          itemCount: listArticles.length,
+                        );
+                      }
+                    }
+                    return Container();
+                  },
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
